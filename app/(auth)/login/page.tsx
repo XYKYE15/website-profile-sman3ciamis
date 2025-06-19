ee594708d7701dc072d9260632cfec910bc7619c
xykye15 <rizkirenaldi115@gmail.com>
2025-06-19 15:00:11 +0700

    update

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import AuthLayout from "@/components/auth/AuthLayout";
 import LoginForm from "@/components/auth/LoginForm";
 
 const LoginPage = () => {
   return (
     <AuthLayout title="" showOr={true}>
-      <LoginForm />
+      <Suspense fallback={<div>Loading login form...</div>}>
+        <LoginForm />
+      </Suspense>
     </AuthLayout>
   );
 };
