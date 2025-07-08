d2d1a7ab9ab762e234fb659ad746693247f48710
xykye15 <rizkirenaldi115@gmail.com>
2025-07-08 12:25:57 +0700

    memperbaiki kesalahan pesan pada login admin panel

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,21 +1,11 @@
-"use client";
-
-import dynamic from "next/dynamic";
 import { Suspense } from "react";
 import AuthLayout from "@/components/auth/AuthLayout";
-import LogoutNotification from "@/components/admin/LogoutNotif";
-
-
-const LoginForm = dynamic(() => import("@/components/auth/LoginForm"), {
-  ssr: false,
-  loading: () => <div>Memuat formulir login...</div>,
-});
+import LoginForm from "@/components/auth/LoginForm";
 
 const LoginPage = () => {
   return (
     <AuthLayout title="" showOr={true}>
-      <LogoutNotification />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading login form...</div>}>
         <LoginForm />
       </Suspense>
     </AuthLayout>
